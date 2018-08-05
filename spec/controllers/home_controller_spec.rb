f20ff1e8245f3a27dec4804bc6fc07a60b183f04
Alberto Rocha <betogrun@gmail.com>
2018-08-05 12:35:36 -0300

    Upgrade rails to version 5.2

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -24,6 +24,7 @@ RSpec.describe HomeController, type: :controller do
     it "returns a numeric value" do
       post :exchange, params: {currency: @currency, currency_destination: @currency_destination, quantity: @quantity}
       value = JSON.parse(response.body)['value']
+      puts response.body
       expect(value.is_a? Numeric).to eql(true)
     end
   end
